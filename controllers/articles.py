207e4d63f84e1e0aabbca7b6bf8c659fa5cc64be
David Hariri <david.hariri@gmail.com>
2016-02-29 23:05:35 -0500

    Wrote a proper Article class and updated methods

--- controllers/articles.py
+++ controllers/articles.py
@@ -2,103 +2,117 @@ import pymongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 import json
-import datetime
+from datetime import datetime
 from config import settings
-import markdown
+from markdown import markdown as HTML_from_markdown
 
 database = pymongo.MongoClient('mongodb://{}:{}{}'.format(settings["database"]["user"], settings["database"]["pass"], settings["database"]["url"]))["blog"]
 
-def Article(article):
-    article = json.loads(dumps(article))
-
-    return {
-        "id" : article["_id"]["$oid"],
-        "key" : article["key"],
-        "title" : article["title"],
-        "content" : article["content"],
-        "made" : article["made"]["$date"],
-        "updated" : article["updated"]["$date"],
-        "tags" : article["tags"],
-        "published" : article["published"]
-    }
-
-def parse_articles(articles_from_db):
-    articles = []
-
-    for article in articles_from_db:
-        articles.append(Article(article))
-
-    return articles
-
-def parse_content(markdown_content):
-    html_content = markdown.markdown(markdown_content)
-
-    return {
-        "markdown" : markdown_content, # Raw markdown supported text
-        "html" : html_content
-    }
-
-def find(key=None, tag=None, only_published=True):
-    # Find by id (favoured over tag)
-    if key != None:
-        if only_published:
-            return parse_articles(database.articles.find({"key" : key, "published" : True}).limit(1))
-        else:
-            return parse_articles(database.articles.find({"key" : key}).limit(1))
-
-    # Find by tag
-    if tag != None:
-        if only_published:
-            return parse_articles(database.articles.find({"tags" : tag, "published" : True}).limit(0))
-        else:
-            return parse_articles(database.articles.find({"tags" : tag}).limit(0))
-
-    # Default response to find()
-    if only_published:
-        return parse_articles(database.articles.find({"published" : True}).sort("made", -1).limit(0))
-    else:
-        return parse_articles(database.articles.find().sort("made", -1).limit(0))
-
-def new(key="", title="", content="", tags=None):
-    now = datetime.datetime.utcnow()
-
-    article = {
-        "key" : key,
-        "title" : title,
-        "content" : parse_content(content),
-        "tags" : tags,
-        "updated" : now,
-        "made" : now,
-        "published" : False
-    }
-
-    article["id"] = database.articles.insert_one(article).inserted_id
-
-    return Article(article)
-
-def replace(article):
-    parsed_content = parse_content(article["content"]["markdown"])
-    database.articles.update(
-        {
-            "_id" : ObjectId(article["id"])
-        },
-        {
-            "$set" : {
-                "title" : article["title"],
-                "key" : article["key"],
-                "update" : datetime.datetime.utcnow(),
-                "content" : parsed_content,
-                "published" : article["published"],
-                "tags" : article["tags"]
+class Article(object):
+    def __init__(self, _id=ObjectId(), key=None, title="", text="", made=datetime.now(), updated=datetime.now(), tags=[], published=False, content=None):
+        if content == None:
+            self.content = {
+                "html" : "",
+                "markdown" : text
             }
-        },
-        False
-    )
+        else:
+            self.content = content
 
-    article["content"] = parsed_content
-    return article
+        if isinstance(_id, ObjectId):
+            self._id = _id
+        elif isinstance(_id, basestring):
+            self._id = ObjectId(_id)
+        else:
+            self._id = ObjectId()
 
-def delete(id):
-    database.articles.delete_one({"_id" : ObjectId(id)})
+        self.key = key
+        self.title = title
 
-    return True
+    def set_text(self, new_text):
+        self.content["markdown"] = new_text
+        self.content["html"] = HTML_from_markdown(new_text)
+        self.updated = datetime.now()
+
+    def set_title(self, new_title):
+        self.title = new_title
+        self.updated = datetime.now()
+
+    def set_key(self, new_key):
+        self.key = new_key
+        self.updated = datetime.now()
+
+    def set_published(self, new_published):
+        self.published = new_published
+        self.updated = datetime.now()
+
+    def get_json(self):
+        return dumps(self)
+
+def find(_id=None, key=None, only_published=True):
+    # Find an article in various ways and return a list of Article's
+    # Favoring _id > key
+
+    # Append a published flag if specified
+    def get_published_flag():
+        if only_published:
+            return {"published" : True}.items()
+
+        return {}.items()
+
+    def get_search_results_from_search(search):
+        results = []
+
+        try:
+            db_results = database.articles.find(search)
+
+            for result in db_results:
+                results.append(Article(**result))
+
+        except Exception as e:
+            print "Could not make search:"
+            print e
+
+        return results
+
+    if isinstance(_id, ObjectId):
+        search = dict({"_id" : _id}.items() + get_published_flag())
+        return get_search_results_from_search(search)
+
+    elif isinstance(key, basestring):
+        search = dict({"key" : key}.items() + get_published_flag())
+        return get_search_results_from_search(search)
+
+    else:
+        print "Cannot find article. No searchable attribute passed"
+
+    return None
+
+def save(article):
+    if isinstance(article, Article):
+        try:
+            database.articles.update({"_id" : article._id}, {"$set" : article.__dict__}, True)
+            return article
+
+        except Exception as e:
+            print "Could not save Article. Database Error:"
+            print e
+
+    else:
+        print "Could not save Article. Object not of type Article"
+
+    return False
+
+def delete(article):
+    if isinstance(article, Article):
+        try:
+            database.articles.delete_one({"_id" : article._id})
+            return article
+
+        except Exception as e:
+            print "Could not delete Article. Database Error:"
+            print e
+
+    else:
+        print "Could not delete Article. Object not of type Article"
+
+    return False
