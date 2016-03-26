2fa0a2ffca8c0254622ecbf7fb9aeb29b04cc7ca
David Hariri <david.hariri@gmail.com>
2016-03-26 11:16:38 -0400

    added read_count parameter to Article model

--- controllers/articles.py
+++ controllers/articles.py
@@ -9,7 +9,7 @@ import json
 database = pymongo.MongoClient('mongodb://{}:{}{}'.format(settings["database"]["user"], settings["database"]["pass"], settings["database"]["url"]))["blog"]
 
 class Article(object):
-    def __init__(self, _id=ObjectId(), title="", made=None, updated=None, tags=[], published=False, shared=False, content=None, **kwargs):
+    def __init__(self, _id=ObjectId(), title="", made=None, updated=None, tags=[], published=False, shared=False, content=None, read_count=0, **kwargs):
         # Content checker
         if isinstance(content, dict) is False:
             self.content = {
@@ -50,6 +50,7 @@ class Article(object):
         self.tags = tags
         self.published = published
         self.shared = shared
+        self.read_count = read_count
 
     def render_html(self):
         self.content["html"] = HTML_from_markdown(self.content["markdown"], extensions=["fenced_code"])
