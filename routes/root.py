dfaa655db7e3af3e66e2e0808940c1216446617f
David Hariri <david.hariri@gmail.com>
2017-07-01 17:26:24 -0400

    Adds auth to root endpoint for authentication

--- routes/root.py
+++ routes/root.py
@@ -1,10 +1,14 @@
 from flask_restful import Resource
-
+from helpers.auth import security
 
 class RootEndpoint(Resource):
     """
     Routes defined for manipluating Article objects
     """
-    def get(self):
+    @security()
+    def get(self, authorized):
         """Base endpoint"""
-        return {"message": "What hath God wrought?"}, 200
+        if authorized:
+            return {"message": "What hath God wrought?"}, 200
+        else:
+            return {"message": "Hello World"}, 200
