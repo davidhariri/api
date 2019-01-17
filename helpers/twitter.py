48aa665057bd86136d83af85a716ff81b607d587
David Hariri <david.hariri@gmail.com>
2019-01-17 15:58:57 -0500

    Uses optimized links for Twitter media posts

--- helpers/twitter.py
+++ helpers/twitter.py
@@ -10,6 +10,20 @@ twitter_api = twitter.Api(consumer_key=os.getenv("TWITTER_KEY"),
 	access_token_key=os.getenv("TWITTER_ACCESS_KEY"),
 	access_token_secret=os.getenv("TWITTER_ACCESS_SECRET"))
 
+def process_media(media):
+	optimized_urls = []
+
+	for url in media:
+		file_type = url.split(".")[-1]
+
+		if file_type in ["gif"]:
+			optimized_urls.append(url.replace(".gif", ".thumb.mp4"))
+
+		elif file_type in ["jpg", "jpeg", "png"]:
+			optimized_urls.append(url.replace(".{}".format(file_type), ".thumb.jpeg"))
+	
+	return optimized_urls
+
 def post_post_as_tweet(post):
 	"""
 	Takes a post and formats it as a tweet, then sends the tweet to Twitter
@@ -31,7 +45,7 @@ def post_post_as_tweet(post):
 
 	if post.media:
 		try:
-			return twitter_api.PostUpdate(status, media=post.media[:4])
+			return twitter_api.PostUpdate(status, media=process_media(post.media)[:4])
 		except Exception as e:
 			capture_exception(e)
 			
